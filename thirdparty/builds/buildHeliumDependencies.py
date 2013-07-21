4c80bb05e3c9604f697944729f0293fc977bba6d
AKindyakov <akindyakov@gmail.com>
2013-07-21 18:27:12 +0400

    add unzip

--- thirdparty/builds/buildHeliumDependencies.py
+++ thirdparty/builds/buildHeliumDependencies.py
@@ -6,23 +6,31 @@ import os
 import sys
 import time
 import zipfile
-import subsystem
+import subprocess
 
-log_file = open("heluim_thirdparty_buildlog_""time.strftime("%m.%d_%H.%M.%S")" + '.log', 'a')
+log_file = open("heluim_thirdparty_buildlog_"+time.strftime("%m.%d_%H.%M.%S") + '.log', 'a')
 
+roodScriptName = os.path.abspath(os.path.dirname(__file__))
+print(roodScriptName)
 
+luaHeliumDir = os.path.normpath(roodScriptName + '/../lua')
+luaArchiveWithSrcs = os.path.expanduser("~/Dropbox/game_project/helium_resource/thirdparty_libraries/lua-5.2.2.zip")
+print(luaHeliumDir)
 
-luaArchiveWithSrcs = os.expanduser("~/Dropbox/game_project/helium_resource/thirdparty_libraries/lua-5.2.2.zip")
-luaArchiveWithSrcs = os.expanduser("~/Dropbox/game_project/helium_resource/thirdparty_libraries/lua-5.2.2.zip")
+#os.path.normpath()
 
-def luaDownloadAndBuild ()
-    
+def luaDownloadAndBuild ():
+    luaArch = zipfile.ZipFile(luaArchiveWithSrcs, "r")
+    luaArch.extractall()
     return
 
 try:
+    luaDownloadAndBuild ()
 
-
-
+except Exception as e:
+    print("Something wrong ...")
+    print(e)
 
 finally:
+    log_file.close()
 
