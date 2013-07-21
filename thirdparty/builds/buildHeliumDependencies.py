ef5d7eb17a2750bff1a93524f27f493b2c001501
AKindyakov <akindyakov@gmail.com>
2013-07-21 18:12:52 +0400

    add

---
+++ thirdparty/builds/buildHeliumDependencies.py
@@ -0,0 +1,28 @@
+#-*- utf-8 -*-
+
+
+
+import os
+import sys
+import time
+import zipfile
+import subsystem
+
+log_file = open("heluim_thirdparty_buildlog_""time.strftime("%m.%d_%H.%M.%S")" + '.log', 'a')
+
+
+
+luaArchiveWithSrcs = os.expanduser("~/Dropbox/game_project/helium_resource/thirdparty_libraries/lua-5.2.2.zip")
+luaArchiveWithSrcs = os.expanduser("~/Dropbox/game_project/helium_resource/thirdparty_libraries/lua-5.2.2.zip")
+
+def luaDownloadAndBuild ()
+    
+    return
+
+try:
+
+
+
+
+finally:
+
