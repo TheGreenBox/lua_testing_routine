62dfd37e5f2f76d6569210e410a8f8b738e594b7
AKindyakov <akindyakov@gmail.com>
2013-07-21 19:13:18 +0400

    add make and make instal command to lua build function

--- thirdparty/builds/buildHeliumDependencies.py
+++ thirdparty/builds/buildHeliumDependencies.py
@@ -7,29 +7,112 @@ import sys
 import time
 import zipfile
 import subprocess
+import shutil
 
-log_file = open("heluim_thirdparty_buildlog_"+time.strftime("%m.%d_%H.%M.%S") + '.log', 'a')
+rootScriptName = os.path.abspath(os.path.dirname(__file__))
+outputDirName = rootScriptName+'/product'
+print(rootScriptName)
 
-roodScriptName = os.path.abspath(os.path.dirname(__file__))
-print(roodScriptName)
-
-luaHeliumDir = os.path.normpath(roodScriptName + '/../lua')
+luaHeliumDir = os.path.normpath(rootScriptName + '/../lua')
 luaArchiveWithSrcs = os.path.expanduser("~/Dropbox/game_project/helium_resource/thirdparty_libraries/lua-5.2.2.zip")
-print(luaHeliumDir)
+
+if not os.path.isdir(rootScriptName+'/log'):
+    os.mkdir(rootScriptName+'/log')
+
+log_file = open("log/heluim_thirdparty_buildlog_"+time.strftime("%m.%d_%H.%M.%S") + '.log', 'a')
 
 #os.path.normpath()
 
-def luaDownloadAndBuild ():
-    luaArch = zipfile.ZipFile(luaArchiveWithSrcs, "r")
-    luaArch.extractall()
+def luaDownloadAndBuild ( buildType ):
+    
+    luaHeliumWorkingDir = os.path.normpath(rootScriptName + '/../lua/build')
+    log_file.write('Lua ' + buildType + ' building ')
+    print('Lua ' + buildType + ' building ')
+    
+    luaArch = zipfile.ZipFile(luaArchiveWithSrcs, 'r')
+    luaArch.extractall(luaHeliumDir)
+    
+    log_file.write('downloaded: '+luaArchiveWithSrcs)
+    print('downloaded: '+luaArchiveWithSrcs)
+    log_file.write('and extracted to: '+luaHeliumDir)
+    print('and extracted to: '+luaHeliumDir)
+    
+    if os.path.isdir(luaHeliumWorkingDir):
+        shutil.rmtree(luaHeliumWorkingDir)
+        
+    os.mkdir(luaHeliumWorkingDir)
+    cmakeBuildType = ''
+    if buildType == 'Release' or builType == 'Debug':
+        luaHeliumWorkingDir = luaHeliumWorkingDir + '/' + buildType
+        os.mkdir(luaHeliumWorkingDir)
+        cmakeBuildType = '-DCMAKE_BUILD_TYPE='+buildType
+    else:
+        buildType = ''
+    
+    cmakeGenerator = ''
+    if sys.platform == 'linux2':
+        cmakeGenerator = '-GUnix Makefiles'
+    elif sys.platform == 'win32':
+        cmakeGenerator = '-GVisual Studio 11'
+        
+    os.chdir(luaHeliumWorkingDir)
+
+    cmakeInstallParam = '-DHELIUM_THIRD_PARTY_OUTPUT_PATH='+outputDirName
+    luaCmakeCmd = [ 'cmake',
+                    cmakeGenerator,
+                    cmakeBuildType,
+                    cmakeInstallParam,
+                    luaHeliumDir    ]
+    
+    cmakeProc = subprocess.Popen(luaCmakeCmd, stdout=subprocess.PIPE)
+    log, logEr = cmakeProc.communicate()
+    
+    if log:
+        log_file.write(log)
+        print("Stdout:\n")
+        print(log)
+        
+    if logEr:
+        log_file.write(logEr)
+        print("Error out:\n")
+        print(logEr)
+
+    makeProc = subprocess.Popen(['make'], stdout=subprocess.PIPE)
+    log, logEr = makeProc.communicate()
+
+    if log:
+        log_file.write(log)
+        print("Stdout:\n")
+        print(log)
+        
+    if logEr:
+        log_file.write(logEr)
+        print("Error out:\n")
+        print(logEr)
+
+    makeInstallProc = subprocess.Popen(['make', 'install'], stdout=subprocess.PIPE)
+    log, logEr = makeInstallProc.communicate()
+    
+    if log:
+        log_file.write(log)
+        print("Stdout:\n")
+        print(log)
+        
+    if logEr:
+        log_file.write(logEr)
+        print("Error out:\n")
+        print(logEr)
+    
+    os.chdir(rootScriptName)
     return
 
 try:
-    luaDownloadAndBuild ()
+   luaDownloadAndBuild ('Release')
+   # luaDownloadAndBuild ('Debug')
 
-except Exception as e:
-    print("Something wrong ...")
-    print(e)
+#except Exception as e:
+#    print("Something wrong ...")
+#    print(e)
 
 finally:
     log_file.close()
