da881bc4e26fc0688be0f2a439a9380eaf2ae265
AKindyakov <akindyakov@gmail.com>
2013-07-27 15:29:59 +0400

    add common include for lua includes files

---
+++ thirdparty/lua/luaIncludes.h
@@ -0,0 +1,17 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Helium
+ * File name:  luaIncludes.h
+ * Description:
+ * Author:  AKindyakov 
+ * ========================================================
+ */
+
+extern "C"
+{
+    #include "lua.h"
+    #include "lauxlib.h"
+    #include "lualib.h"
+}
+
